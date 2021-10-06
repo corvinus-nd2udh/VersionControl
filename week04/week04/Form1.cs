5c71d569af3afe9a3b5642ce4301c754396a6528
Bence Dosztár <bence.dosztar@stud.uni-corvinus.hu>
2021-10-06 16:50:08 +0200

    CreateExcel függvény

--- week04/week04/Form1.cs
+++ week04/week04/Form1.cs
@@ -25,10 +25,35 @@ namespace week04
         {
             InitializeComponent();
             LoadData();
+            CreateExcel();
         }
         private void LoadData()
         {
             Flats = context.Flats.ToList();
         }
+
+        private void CreateExcel()
+        {
+            try
+            {
+                xlApp = new Excel.Application();
+                xlWB = xlApp.Workbooks.Add(Missing.Value);
+                xlSheet = xlWB.ActiveSheet;
+                //CreateTable();
+                xlApp.Visible = true;
+                xlApp.UserControl = true;
+            }
+            catch (Exception ex)
+            {
+                string errMsg = string.Format("Error: {0}\nLine: {1}", ex.Message, ex.Source);
+                MessageBox.Show(errMsg, "Error");
+                xlWB.Close(false, Type.Missing, Type.Missing);
+                xlApp.Quit();
+                xlWB = null;
+                xlApp = null;
+                throw;
+            }
+        }
+
     }
 }
