51d202e6426331136f79039eb30d51e79a9f2884
Bence Dosztár <bence.dosztar@stud.uni-corvinus.hu>
2021-10-20 15:06:25 +0200

    VaR kiszamitasa

--- week05/week05/Form1.cs
+++ week05/week05/Form1.cs
@@ -22,6 +22,20 @@ namespace week05
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
             CreatePortfolio();
+
+            List<decimal> Nyereségek = new List<decimal>();
+            int intervallum = 30;
+            DateTime kezdoDatum = (from x in Ticks select x.TradingDay).Min();
+            DateTime zaroDatum = new DateTime(2016, 12, 30);
+            TimeSpan z = zaroDatum - kezdoDatum;
+            for (int i = 0; i < z.Days - intervallum; i++)
+            {
+                decimal ny = GetPortfolioValue(kezdoDatum.AddDays(1 + intervallum)) - GetPortfolioValue(kezdoDatum.AddDays(i));
+                Nyereségek.Add(ny);
+                Console.WriteLine(i + " " + ny);
+            }
+            var nyereségekRendezve = (from x in Nyereségek orderby x select x).ToList();
+            MessageBox.Show(nyereségekRendezve[nyereségekRendezve.Count() / 5].ToString());
         }
 
         private void CreatePortfolio()
