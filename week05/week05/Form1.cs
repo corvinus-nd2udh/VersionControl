6436ff2c169442ab50318def60757a074f5930d0
Bence Dosztár <bence.dosztar@stud.uni-corvinus.hu>
2021-10-20 14:40:49 +0200

    portfolio tarolasa a memoriaban

--- week05/week05/Form1.cs
+++ week05/week05/Form1.cs
@@ -7,18 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week05.Entities;
 
 namespace week05
 {
     public partial class Form1 : Form
     {
         PortfolioEntities context = new PortfolioEntities();
-        List<Tick> ticks;
+        List<Tick> Ticks;
+        List<PortfolioItem> Portfolio = new List<PortfolioItem>();
         public Form1()
         {
             InitializeComponent();
-            ticks = context.Ticks.ToList();
-            dataGridView1.DataSource = ticks;
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
+            CreatePortfolio();
+        }
+
+        private void CreatePortfolio()
+        {
+            Portfolio.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            Portfolio.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = Portfolio;
         }
     }
 }
