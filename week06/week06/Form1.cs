898b5a5f7bdb781f7e82395e4eeffd924178d6c1
Bence Dosztár <bence.dosztar@stud.uni-corvinus.hu>
2021-10-26 19:36:45 +0200

    3) Webszolg. hívása és 4) Árfolyam lista

--- week06/week06/Form1.cs
+++ week06/week06/Form1.cs
@@ -7,21 +7,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week06.MnbServiceReference;
+using week06.Entities;
 
 
 namespace week06
 {
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
         public Form1()
         {
             InitializeComponent();
             WebService();
+            dataGridView1.DataSource = Rates;
         }
 
         private void WebService()
         {
-            MNBArfoly
+            var mnbService = new MNBArfolyamServiceSoapClient();
+            var request = new GetExchangeRatesRequestBody
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+            var response = mnbService.GetExchangeRates(request);
+            var result = response.GetExchangeRatesResult;
         }
     }
 }
