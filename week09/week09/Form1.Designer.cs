e133d32295a8e2db584a577c7c859b21722f09b5
Bence Dosztár <bence.dosztar@stud.uni-corvinus.hu>
2021-12-01 23:40:03 +0100

    Adatok betöltése, véletlenszám generálás, szimulció

--- week09/week09/Form1.Designer.cs
+++ week09/week09/Form1.Designer.cs
@@ -29,13 +29,121 @@ namespace week09
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDownCloseDate = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxCsvPath = new System.Windows.Forms.TextBox();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCloseDate)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Záróév: ";
+            // 
+            // numericUpDownCloseDate
+            // 
+            this.numericUpDownCloseDate.Location = new System.Drawing.Point(66, 16);
+            this.numericUpDownCloseDate.Maximum = new decimal(new int[] {
+            2050,
+            0,
+            0,
+            0});
+            this.numericUpDownCloseDate.Minimum = new decimal(new int[] {
+            2024,
+            0,
+            0,
+            0});
+            this.numericUpDownCloseDate.Name = "numericUpDownCloseDate";
+            this.numericUpDownCloseDate.Size = new System.Drawing.Size(69, 20);
+            this.numericUpDownCloseDate.TabIndex = 1;
+            this.numericUpDownCloseDate.Value = new decimal(new int[] {
+            2024,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(190, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Népesség fájl:";
+            // 
+            // textBoxCsvPath
+            // 
+            this.textBoxCsvPath.Location = new System.Drawing.Point(270, 15);
+            this.textBoxCsvPath.Name = "textBoxCsvPath";
+            this.textBoxCsvPath.Size = new System.Drawing.Size(304, 20);
+            this.textBoxCsvPath.TabIndex = 3;
+            this.textBoxCsvPath.Text = "C:\\Temp\\nép.csv";
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Location = new System.Drawing.Point(580, 13);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(101, 23);
+            this.buttonBrowse.TabIndex = 4;
+            this.buttonBrowse.Text = "Browse";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.buttonBrowse_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(687, 13);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(101, 23);
+            this.buttonStart.TabIndex = 5;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 47);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(776, 391);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.buttonBrowse);
+            this.Controls.Add(this.textBoxCsvPath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDownCloseDate);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownCloseDate)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDownCloseDate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxCsvPath;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
--- week09/week09/Form1.cs
+++ week09/week09/Form1.cs
@@ -3,18 +3,155 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using week09.Entities;
 
 namespace week09
 {
     public partial class Form1 : Form
     {
+        List<Person> Population = new List<Person>();
+        List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
+        List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
+
+        Random rng = new Random(1234);
         public Form1()
         {
             InitializeComponent();
+
+            Population = GetPopulation(@"C:\Temp\nép-teszt.csv");
+            BirthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
+            DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
+        }
+
+        private void Simulation()
+        {
+            for (int year = 2005; year <= numericUpDownCloseDate.Value; year++)
+            {
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    SimStep(year, Population[i]);
+                }
+
+                int numberOfMales = (from x in Population
+                                     where x.Gender == Gender.Male && x.IsAlive
+                                     select x).Count();
+                int numberOfFemales = (from x in Population
+                                       where x.Gender == Gender.Female && x.IsAlive
+                                       select x).Count();
+                Console.WriteLine(string.Format("Év: {0}, Férfiak: {1}, Nők: {2}", year, numberOfMales, numberOfFemales));
+            }
+        }
+
+        private void SimStep(int year, Person person)
+        {
+            if (!person.IsAlive) return;
+            byte age = (byte)(year - person.BirthYear);
+            double ProbOfDeath = (from x in DeathProbabilities
+                                  where x.Gender == person.Gender && x.Age == age
+                                  select x.ProbabilityOfDeath).FirstOrDefault();
+            if (rng.NextDouble() <= ProbOfDeath) person.IsAlive = false;
+            if (person.IsAlive && person.Gender == Gender.Female)
+            {
+                double ProbOfBirth = (from x in BirthProbabilities
+                                      where x.Age == age && x.NumberOfChildren == person.NumberOfChildren
+                                      select x.ProbabilityOfBirth).FirstOrDefault();
+                if (rng.NextDouble() <= ProbOfBirth)
+                {
+                    Person newBorn = new Person()
+                    {
+                        BirthYear = year,
+                        Gender = (Gender)rng.Next(1, 3),
+                        NumberOfChildren = 0,
+                    };
+                    Population.Add(newBorn);
+                }
+            }
+        }
+
+        public List<Person> GetPopulation(string csvpath)
+        {
+            List<Person> population = new List<Person>();
+
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split(';');
+                    Person p = new Person()
+                    {
+                        BirthYear = int.Parse(line[0]),
+                        Gender = (Gender)Enum.Parse(typeof(Gender), line[1]),
+                        NumberOfChildren = int.Parse(line[2])
+                    };
+                    population.Add(p);
+                }
+            }
+            return population;
+        }
+
+        public List<BirthProbability> GetBirthProbabilities(string csvpath)
+        {
+            List<BirthProbability> births = new List<BirthProbability>();
+
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split(';');
+                    BirthProbability bp = new BirthProbability()
+                    {
+                        Age = int.Parse(line[0]),
+                        NumberOfChildren = int.Parse(line[1]),
+                        ProbabilityOfBirth = double.Parse(line[2], CultureInfo.GetCultureInfo("hu-HU"))
+                    };
+                    births.Add(bp);
+                }
+            }
+            return births;
+        }
+
+        public List<DeathProbability> GetDeathProbabilities(string csvpath)
+        {
+            List<DeathProbability> deaths = new List<DeathProbability>();
+
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split(';');
+                    DeathProbability bp = new DeathProbability()
+                    {
+                        Gender = (Gender)Enum.Parse(typeof(Gender), line[0]),
+                        Age = int.Parse(line[1]),
+                        ProbabilityOfDeath = double.Parse(line[2], CultureInfo.GetCultureInfo("hu-HU"))
+                    };
+                    deaths.Add(bp);
+                }
+            }
+            return deaths;
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            Simulation();
+        }
+
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Comma Separated Values (*.csv)|*.csv|All files (*.*)|*.*";
+            ofd.DefaultExt = ".csv";
+            ofd.AddExtension = true;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+
+            }
         }
     }
 }
