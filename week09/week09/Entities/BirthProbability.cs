6e8b82f419b5d4706dc5ea01563c27b7f69668d7
Bence Dosztár <bence.dosztar@stud.uni-corvinus.hu>
2021-12-01 23:37:58 +0100

    Osztályok létrehozása

---
+++ week09/week09/Entities/BirthProbability.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week09.Entities
+{
+    public class BirthProbability
+    {
+        public int Age { get; set; }
+        public int NumberOfChildren { get; set; }
+        public double ProbabilityOfBirth { get; set; }
+    }
+}
