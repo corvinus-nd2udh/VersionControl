c72c46ca260f6e429b92c4e213046966ff7249be
Bence Dosztár <bence.dosztar@stud.uni-corvinus.hu>
2021-12-09 20:30:26 +0100

    Induló populáció felépítése

--- week10/week10/Form1.cs
+++ week10/week10/Form1.cs
@@ -15,6 +15,11 @@ namespace week10
     {
         GameController gc = new GameController();
         GameArea ga;
+
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
         public Form1()
         {
             InitializeComponent();
@@ -22,6 +27,12 @@ namespace week10
             Controls.Add(ga);
             //gc.AddPlayer();
             //gc.Start(true);
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+            gc.Start();
         }
     }
 }
