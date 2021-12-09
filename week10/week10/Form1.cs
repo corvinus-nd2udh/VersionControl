0ccb674bca7a7961a2d4041429c946bd5f4827d1
Bence Dosztár <bence.dosztar@stud.uni-corvinus.hu>
2021-12-09 21:06:54 +0100

    Győztes kiválasztása és verseny felépítése

--- week10/week10/Form1.cs
+++ week10/week10/Form1.cs
@@ -16,6 +16,8 @@ namespace week10
         GameController gc = new GameController();
         GameArea ga;
 
+        Brain winnerBrain = null;
+
         int populationSize = 100;
         int nbrOfSteps = 10;
         int nbrOfStepsIncrement = 10;
@@ -46,6 +48,17 @@ namespace week10
                              select p;
             var topPerformers = playerList.Take(populationSize / 2).ToList();
 
+            var winners = from p in topPerformers
+                          where p.IsWinner
+                          select p;
+            if (winners.Count() > 0)
+            {
+                winnerBrain = winners.FirstOrDefault().Brain.Clone();
+                gc.GameOver -= Gc_GameOver;
+                button1.Visible = true;
+                return;
+            }
+
             gc.ResetCurrentLevel();
             foreach (var p in topPerformers)
             {
@@ -61,6 +74,15 @@ namespace week10
                     gc.AddPlayer(b.Mutate());
             }
             gc.Start();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gc.ResetCurrentLevel();
+            gc.AddPlayer(winnerBrain.Clone());
+            gc.AddPlayer();
+            ga.Focus();
+            gc.Start(true);
         }
     }
 }
